73e1cf7bf96e4a48621f40aadb8876b1f2d5b8dd
imadvs <imadvs00@gmail.com>
2026-02-06 15:55:16 +0100

    Fixing MX Master scrolling

---
+++ Hyprland-Dots/config/nvim/lua/plugins/precognition.lua
@@ -0,0 +1,8 @@
+return {
+  "tris203/precognition.nvim",
+  -- event = "VeryLazy", -- Optional: load later if needed
+  config = {
+    startVisible = true, -- Optional: start with hints visible
+    -- showBlankVirtLine = true,
+  },
+}
